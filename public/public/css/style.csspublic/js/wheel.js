fa65cfe80a89947e35246c6ef8c4477cd341dd41
headaim121-sys <headaim121@gmail.com>
2026-09-25 11:53:08 +0700

    Create wheel.js

---
+++ public/public/css/style.csspublic/js/wheel.js
@@ -0,0 +1,15 @@
+lucky-wheel
+│
+├── data
+├── public
+│   ├── assets
+│   ├── css
+│   ├── js
+│   ├── admin.html
+│   └── index.html
+│
+├── server
+├── .env.example
+├── .gitignore
+├── README.md
+└── package.json
